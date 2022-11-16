580b182aad0dcc890c89f846d1277492602e7a42
David Fernandez <100215276+Davfer20@users.noreply.github.com>
2022-11-15 22:18:16 -0600

    Proyecto Terminado

--- RobotWars/src/robotwar/communication/SocketServer.java
+++ RobotWars/src/robotwar/communication/SocketServer.java
@@ -1,40 +1,65 @@
 package robotwar.communication;
 
+import java.io.InputStreamReader;
+
+import java.io.BufferedReader;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class SocketServer implements Runnable {
-	private final int PORT_NUMBER = 10000;
-	private boolean listening;
 
-	public SocketServer() {
-		listening = true;
-	}
-
-	public void startListening() {
-		Thread t = new Thread(this);
-		t.start();
-	}
-
-	public void stopListening() {
-		listening = false;
-
-	}
-
-	public void run() {
-		try {
-			// este es el server especifico que escucha los entrantes
-			ServerSocket socketListener = new ServerSocket(PORT_NUMBER);
-
-			while (listening) {
-
-				// la conexion del que se acaba de conectar
-				Socket connection = socketListener.accept();
-				SocketClient client = new SocketClient(connection);
-			}
-
-		} catch (Exception ex) {
-			ex.printStackTrace();
+public class SocketServer{
+	
+	public static void main(String[] args) throws IOException {
+		Socket connection2 = null;
+		ServerSocket serverSocket = null;
+		InputStreamReader inputStreamReader = null;
+		OutputStreamWriter outputStreamWriter = null;
+		BufferedReader bufferedReader = null;
+		BufferedWriter bufferedWriter = null;
+		
+		
+		serverSocket = new ServerSocket (2070);
+		
+		while (true)
+		{
+			try
+			{
+				connection2 = serverSocket.accept();
+				
+				inputStreamReader = new InputStreamReader(connection2.getInputStream());
+				outputStreamWriter = new OutputStreamWriter(connection2.getOutputStream());
+				
+				bufferedReader = new BufferedReader(inputStreamReader);
+				bufferedWriter = new BufferedWriter(outputStreamWriter);
+				
+				while (true)
+				{
+					String msgClient = bufferedReader.readLine();
+					
+					System.out.println("Client: "+msgClient);
+					
+					bufferedWriter.write("MsgRecived :)" );
+					bufferedWriter.newLine();
+					bufferedWriter.flush();
+					
+					if (msgClient.equals("Adios"))
+						break;
+					
+				}
+				
+				connection2.close();
+				inputStreamReader.close();
+				outputStreamWriter.close();
+				bufferedReader.close();
+				bufferedWriter.close();
+				
+			}catch (IOException e)
+			{
+				e.printStackTrace();}
 		}
 	}
 }
